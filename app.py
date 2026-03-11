5d90986b10f48e289f38b8eb7928c97af4ea2b86
atnsupport <support@atn-solutions.ch>
2026-03-11 08:28:59 +0100

    Update app.py

--- app.py
+++ app.py
@@ -184,5 +184,5 @@ def serve_video(filename):
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-   app.run(host="0.0.0.0", port=port)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
